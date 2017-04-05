2d1c7164aef48603ffb55ca88a274c2d68fa5e6a
halvves <hi@halvves.com>
2017-04-05 13:22:46 -0500

    convert and modulize THREE.VRControls

---
+++ vr-controls.js
@@ -0,0 +1,119 @@
+import {Matrix4} from 'three';
+
+    /**
+ * @author dmarcos / https://github.com/dmarcos
+ * @author mrdoob / http://mrdoob.com
+ * @author halvves / https://github.com/halvves (i only es6 moduled it)
+ */
+
+export default class VRControls {
+  constructor(camera, onError) {
+    this.camera = camera;
+    this.vrDisplay;
+    this.vrDisplays;
+    this.standingMatrix = new Matrix4();
+    this.frameData = null;
+
+    if ('VRFrameData' in window) {
+      this.frameData = new VRFrameData();
+    }
+
+    if (navigator.getVRDisplays) {
+      navigator
+        .getVRDisplays()
+        .then((displays) => {
+          this.vrDisplays = displays;
+          if (displays.length > 0) {
+            this.vrDisplay = displays[0];
+          } else {
+            if (onError) onError('VR input not available.');
+          }
+        })
+        .catch(() => {
+          console.warn('VRControls: Unable to get VR Displays');
+        });
+    }
+
+    // the Rift SDK returns the position in meters
+    // this scale factor allows the user to define how meters
+    // are converted to scene units.
+    this.scale = 1;
+
+    // If true will use "standing space" coordinate system where y=0 is the
+    // floor and x=0, z=0 is the center of the room.
+    this.standing = false;
+
+    // Distance from the users eyes to the floor in meters. Used when
+    // standing=true but the VRDisplay doesn't provide stageParameters.
+    this.userHeight = 1.6;
+  }
+
+  getVRDisplay() {
+    return this.vrDisplay;
+  };
+
+  setVRDisplay(value) {
+    this.vrDisplay = value;
+  };
+
+  getVRDisplays() {
+    console.warn('VRControls: getVRDisplays() is being deprecated.');
+    return this.vrDisplays;
+  };
+
+  getStandingMatrix() {
+    return this.standingMatrix;
+  };
+
+  update() {
+    const camera = this.camera;
+
+    if (this.vrDisplay) {
+      let pose;
+      if (this.vrDisplay.getFrameData) {
+        this.vrDisplay.getFrameData(this.frameData);
+        pose = this.frameData.pose;
+      } else if (this.vrDisplay.getPose) {
+        pose = this.vrDisplay.getPose();
+      }
+      if (pose.orientation !== null) {
+        camera.quaternion.fromArray(pose.orientation);
+      }
+      if (pose.position !== null) {
+        camera.position.fromArray(pose.position);
+      } else {
+        camera.position.set(0, 0, 0);
+      }
+      if (this.standing) {
+        if (this.vrDisplay.stageParameters) {
+          camera.updateMatrix();
+          this.standingMatrix.fromArray(this.vrDisplay.stageParameters.sittingToStandingTransform);
+          camera.applyMatrix(this.standingMatrix);
+        } else {
+          camera.position.setY(camera.position.y + this.userHeight);
+        }
+      }
+      camera.position.multiplyScalar(this.scale);
+    }
+  };
+
+  resetPose() {
+    if (this.vrDisplay) {
+      this.vrDisplay.resetPose();
+    }
+  };
+
+  resetSensor() {
+    console.warn('VRControls: .resetSensor() is now .resetPose().');
+    this.resetPose();
+  };
+
+  zeroSensor() {
+    console.warn('VRControls: .zeroSensor() is now .resetPose().');
+    this.resetPose();
+  };
+
+  dispose() {
+    this.vrDisplay = null;
+  };
+};
